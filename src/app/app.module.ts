a6f32299a676c3c5a601583a32327cee26cece33
AYMERIC <ay.neumann@laposte.net>
2018-12-06 17:16:03 +0100

    app.module.ts cleaned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+//Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BuyerHomeComponent } from './Buyer/buyer-home/buyer-home.component';
 import { SearchVhlComponent } from './Buyer/buyer-home/search-vhl/search-vhl.component';
 import { SearchImmatComponent } from './Buyer/buyer-home/search-immat/search-immat.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './All/navbar/navbar.component';
 import { SearchBarComponent } from './Buyer/buyer-home/search-bar/search-bar.component';
 import { FooterComponent } from './All/footer/footer.component';
 import { PanierComponent } from './Buyer/panier/panier.component';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule, MatInput } from '@angular/material/input';
-import { MatTableModule } from '@angular/material/table';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { ConnexionComponent } from './Sign-up/connexion/connexion.component';
 import { CreationCompteComponent } from './Sign-up/creation-compte/creation-compte.component';
 import { AnnouncePieceComponent } from './Buyer/announce-piece/announce-piece.component';
@@ -30,7 +25,6 @@ import { InfospersoComponent } from './Sign-up/my-account/infosperso/infosperso.
 import { MotdepasseComponent } from './Sign-up/my-account/motdepasse/motdepasse.component';
 import { CommandesComponent } from './Sign-up/my-account/commandes/commandes.component';
 import { AdressesComponent } from './Sign-up/my-account/adresses/adresses.component';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
 import { CategoriesComponent } from './Buyer/categories/categories.component';
 import { SousCategoriesComponent } from './Buyer/categories/sous-categories/sous-categories.component';
 import { SousSousCategoriesComponent } from './Buyer/categories/sous-categories/sous-sous-categories/sous-sous-categories.component';
@@ -40,6 +34,21 @@ import { AdminBuyerListComponent } from './PageAdmin/admin-buyer-list/admin-buye
 import { AdminCompanyListComponent } from './PageAdmin/admin-company-list/admin-company-list.component';
 import { AdminAnnounceListComponent } from './PageAdmin/admin-announce-list/admin-announce-list.component';
 import { DataBaseManagmentComponent } from './PageAdmin/data-base-managment/data-base-managment.component';
+//Material Angular
+import { MatToolbarModule, 
+  MatButtonModule, 
+  MatSidenavModule, 
+  MatIconModule, 
+  MatListModule, 
+  MatCardModule,
+  MatStepperModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatInput,
+  MatTableModule,
+  MatCheckboxModule,
+  MatExpansionModule,
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -93,7 +102,7 @@ import { DataBaseManagmentComponent } from './PageAdmin/data-base-managment/data
     MatIconModule,
     MatListModule,
     MatCardModule,
-    ScrollDispatchModule
+    ScrollDispatchModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

fdf92aa49987c2cda04038dd9e445da3ab8979ba
Julien sch <julien.schader@laposte.net>
2018-12-07 11:01:28 +0100

    creation page annonce pro

--- src/app/Seller/page-announce-pro/tab-creation/tab-creation.component.ts
+++ src/app/Seller/page-announce-pro/tab-creation/tab-creation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
+import { CategoriesService } from '../../../Services/categories.service';
+
 
 @Component({
   selector: 'app-tab-creation',
@@ -11,12 +13,33 @@ export class TabCreationComponent implements OnInit {
 
   currentRate = 0;
 
-  constructor(config: NgbRatingConfig) {
+  constructor(config: NgbRatingConfig, private categoriesService: CategoriesService) {
     // customize default values of ratings used by this component tree
     config.max = 5;
   }
 
+  motoCategories= this.categoriesService.mockMotoCategories;
+
   ngOnInit() {
+  }
+
+  openNav() {
+    let sideNav=document.getElementById("sidenav");
+      sideNav.style.width="300px";
+  }
+
+  closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+  } 
+
+  collapse(index){
+    let categ=document.getElementById('categ'+index)
+    if (categ.style.display=="block"){
+    categ.style.display ="none";
+  }
+    else {
+      categ.style.display="block";
+    }
   }
 
 }

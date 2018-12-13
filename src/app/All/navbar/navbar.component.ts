e6875c723d51b99bcac7fb62dabd2496eeab9676
Florent Perticoz <perticoz@hotmail.com>
2018-12-13 09:43:28 +0100

    form connexion

--- src/app/All/navbar/navbar.component.ts
+++ src/app/All/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BuyerService } from 'src/app/Services/buyer.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { UserConnection } from 'src/app/models/user.connection.model';
 
 @Component({
   selector: 'app-navbar',
@@ -11,18 +13,47 @@ export class NavbarComponent implements OnInit {
 
   mockNumberOfArticles=1;
   modal;
+  registerForm : FormGroup;
+  submitted: boolean = false;
   
   
   
-  constructor(private buyerService: BuyerService, private router: Router) { }
+  constructor(private buyerService: BuyerService, private router: Router, private formBuilder: FormBuilder) { }
 
-  ngOnInit() { console.log(this.buyerConnected); console.log("merde");}
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group ({
+      email: ['', [Validators.required, Validators.email]],
+      password:['', [Validators.required, Validators.minLength(8)]],
+    });
+   }
   
   buyerConnected = this.buyerService.buyerConnected;
    
-  click() {if (this.buyerConnected==undefined) {
+  click() {
+  if (this.buyerConnected==undefined) {
     this.modal="myModal";
   }
   else{this.router.navigate(['/myaccount/infosperso']) }
-}
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+
+    const formValue = this.registerForm.value;
+     const newUserConnection = new UserConnection(
+        0,
+       formValue['email'],
+       formValue['password'],
+     );
+    alert('SUCCESS!! :-)');
+    console.log(this.registerForm.value);
+     console.log(newUserConnection);
+     }
+
 }
---
+++ src/app/models/user.connection.model.ts
@@ -0,0 +1,7 @@
+export class UserConnection {
+    constructor(
+     public id: number, 
+     public email: string,
+     public password: string,
+     ) {}
+ }

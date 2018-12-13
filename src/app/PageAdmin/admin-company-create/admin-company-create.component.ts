058bf2eeb5b3895f650633e2b68ce067479a2d1c
AYMERIC <ay.neumann@laposte.net>
2018-12-13 11:16:53 +0100

    Admin create company form ok need to fix password validator

--- src/app/PageAdmin/admin-company-create/admin-company-create.component.ts
+++ src/app/PageAdmin/admin-company-create/admin-company-create.component.ts
@@ -12,12 +12,68 @@ import { Router } from '@angular/router';
 })
 export class AdminCompanyCreateComponent implements OnInit {
 
-  registerForm: FormGroup;
+  createCompanyForm: FormGroup;
   submitted: boolean = false;
 
   constructor(private formBuilder: FormBuilder, private sellerService: SellerService, private router: Router) { }
 
   ngOnInit() {
+    this.createCompanyForm = this.formBuilder.group ({
+      RaisonSociale: ['', Validators.required],
+      Adressse: ['', Validators.required],
+      CodePostal: ['', Validators.required],
+      Ville: ['', Validators.required],
+      telephone: ['', [Validators.required, Validators.pattern('[0][0-9]{9}') ]],
+      Siret: ['', Validators.required],
+      Siren: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      nomG: ['', Validators.required],
+      prenomG: ['', Validators.required],
+      telephoneG: ['', Validators.required],
+      password:['', [Validators.required, Validators.minLength(8)]],
+    },
+    {
+      // check whether our password and confirm password match
+      validator: CustomValidators.passwordMatchValidator
+    })
   }
+  // convenience getter for easy access to form fields
+  get f() { return this.createCompanyForm.controls; }
 
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.createCompanyForm.invalid) {
+        return;
+    }
+
+    const formValue = this.createCompanyForm.value;
+     const newSeller = new Seller(
+        0,
+       formValue['RaisonSociale'],
+       formValue['Adressse'],
+       formValue['CodePostal'],
+       formValue['Ville'],
+       formValue['telephone'],
+       formValue['Siret'],
+       formValue['Siren'],
+       formValue['email'],
+       formValue['nomG'],
+       formValue['prenomG'],
+       formValue['telephoneG'],
+       formValue['password'],
+      
+     );
+    alert('SUCCESS!! :-)');
+    console.log(this.createCompanyForm.value);
+     console.log(newSeller);
+
+    this.sellerService.createSeller(newSeller)
+    .subscribe(data => (console.log(data), error => console.log(error), this.sellerService.sellerConnected = data,
+    console.log(this.sellerService.sellerConnected.nom),
+    this.router.navigate(['/'])),
+    
+    );
+    
+  }
 }

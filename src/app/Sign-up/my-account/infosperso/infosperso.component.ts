c77e0f11622a0763d06df1dda4e28cd6e0de4cd7
Florent Perticoz <perticoz@hotmail.com>
2018-12-27 18:07:16 +0100

    update compte perso info perso

--- src/app/Sign-up/my-account/infosperso/infosperso.component.ts
+++ src/app/Sign-up/my-account/infosperso/infosperso.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BuyerService } from 'src/app/Services/buyer.service';
+import { Buyer } from 'src/app/models/buyer.models';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-infosperso',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfospersoComponent implements OnInit {
 
-  constructor() { }
+  public buyerConnected
+
+  registerForm: FormGroup;
+  submitted: boolean = false;
+  emailExistant: boolean = false;
+  
+  constructor(private formBuilder: FormBuilder, private buyerService: BuyerService, private router: Router) { }
 
   ngOnInit() {
+    this.buyerConnected = this.buyerService.buyerConnected;
+
+    this.registerForm = this.formBuilder.group ({
+      email: [this.buyerConnected.email, [Validators.required, Validators.email]],
+      prenom: [this.buyerConnected.prenom, Validators.required],
+      nom: [this.buyerConnected.nom, Validators.required],
+      telephone: [this.buyerConnected.telephone, [Validators.required, Validators.pattern('[0][0-9]{9}') ]],
+    })
   }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+      this.submitted = true;
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+      const formValue = this.registerForm.value;
+      const newBuyer = new Buyer(
+        this.buyerConnected.id,
+        formValue['email'] == null? this.buyerConnected.email : formValue['email'],
+        this.buyerConnected.password,
+        this.buyerConnected.civilite,
+        formValue['prenom'],
+        formValue['nom'],
+        formValue['telephone'],
+        this.buyerConnected.adresse1,
+        this.buyerConnected.adresse2,
+        this.buyerConnected.codepostal,
+        this.buyerConnected.ville,
+
+      );
+     alert('SUCCESS!! :-)');
+     console.log(this.registerForm.value);
+      console.log(newBuyer);
+ 
+     this.buyerService.updateBuyer(newBuyer, this.buyerConnected.id)
+     .subscribe(data => 
+                       {console.log(data), this.buyerService.buyerConnected = data,
+     console.log("buyer updated" + this.buyerService.buyerConnected.nom),
+     this.router.navigate(['/'])},
+                error=>{ this.submitted=false,
+                       console.log("erreur!!!"),console.log(error.status), this.emailExistant=true}
+     );
+   }
 
 }

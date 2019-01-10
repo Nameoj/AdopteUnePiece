044f3d5efc2c7bf0697b4f924f2ed2b6cff8e343
Franck MARTINEZ <pacokris@yahoo.fr>
2019-01-10 15:55:08 +0100

    Code Propre

--- src/app/Services/buyer.service.ts
+++ src/app/Services/buyer.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Buyer } from '../models/buyer.models';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
-export const TOKEN = 'token'
-export const AUTHENTICATED_USER = 'authenticaterUser'
+export const TOKEN = 'token';
+export const AUTHENTICATED_USER = 'authenticaterUser';
 
 @Injectable({
   providedIn: 'root'
@@ -13,76 +13,77 @@ export const AUTHENTICATED_USER = 'authenticaterUser'
 
 export class BuyerService {
 
-  public buyerConnected
- 
-  
+  public buyerConnected;
+
+
   private baseUrl = 'http://localhost:8080/api/';
 
   constructor(private http: HttpClient) { }
 
-  getAllBuyers (){
-    console.log("retreiving buyers");
-    return this.http.get<Buyer[]>(`${this.baseUrl}buyers`)
+  getAllBuyers() {
+    console.log('retreiving buyers');
+    return this.http.get<Buyer[]>(`${this.baseUrl}buyers`);
   }
 
   createBuyer(buyer: Object): Observable<Object> {
-    console.log("creation compte");
+    console.log('creation compte');
     return this.http.post<any>(`${this.baseUrl}signup`, buyer)
-    .pipe(
-      map(
-        data => {
-          sessionStorage.setItem(AUTHENTICATED_USER, data.username);
-          sessionStorage.setItem(TOKEN, `Bearer ${data.accessToken}`);
-          console.log("token enregistré" + data.accessToken +" "+ data.username)
-          return data;
-        }
-      )
-    );;
-  };
+      .pipe(
+        map(
+          data => {
+            sessionStorage.setItem(AUTHENTICATED_USER, data.username);
+            sessionStorage.setItem(TOKEN, `Bearer ${data.accessToken}`);
+            console.log('token enregistré' + data.accessToken + ' ' + data.username);
+            return data;
+          }
+        )
+      );
+  }
 
   login(buyer: Object): Observable<Object> {
-    console.log("signin process....");
+    console.log('signin process....');
     return this.http.post<any>(`${this.baseUrl}signin`, buyer)
-    .pipe(
-      map(
-        data => {
-          sessionStorage.setItem(AUTHENTICATED_USER, data.username);
-          sessionStorage.setItem(TOKEN, `Bearer ${data.accessToken}`);
-          console.log("token enregistré" + data.accessToken +" "+ data.username)
-          return data;
-        }
-      )
-    );;
-  };
+      .pipe(
+        map(
+          data => {
+            sessionStorage.setItem(AUTHENTICATED_USER, data.username);
+            sessionStorage.setItem(TOKEN, `Bearer ${data.accessToken}`);
+            console.log('token enregistré' + data.accessToken + ' ' + data.username);
+            return data;
+          }
+        )
+      );
+  }
 
   getBuyerDetails(username): Observable<Object> {
-    console.log("buyer details retreiving");
-    return this.http.get<Buyer>(`${this.baseUrl}buyer/${username}`)
-    
+    console.log('buyer details retreiving');
+    return this.http.get<Buyer>(`${this.baseUrl}buyer/${username}`);
+
   }
 
   getAuthenticatedUser() {
-    return sessionStorage.getItem(AUTHENTICATED_USER)
+    return sessionStorage.getItem(AUTHENTICATED_USER);
   }
 
   getAuthenticatedToken() {
-    if(this.getAuthenticatedUser())
-      return sessionStorage.getItem(TOKEN)
+    if (this.getAuthenticatedUser()) {
+      return sessionStorage.getItem(TOKEN);
+    }
   }
 
   isUserLoggedIn() {
-    let user = sessionStorage.getItem(AUTHENTICATED_USER)
-    return !(user === null)
+    const user = sessionStorage.getItem(AUTHENTICATED_USER);
+    return !(user === null);
   }
 
-  logout(){
-    sessionStorage.removeItem(AUTHENTICATED_USER)
-    sessionStorage.removeItem(TOKEN)
+  logout() {
+    sessionStorage.removeItem(AUTHENTICATED_USER);
+    sessionStorage.removeItem(TOKEN);
   }
 
   updateBuyer(buyer, username): Observable<Object> {
-    console.log("creation compte");
+    console.log('creation compte');
     return this.http.put(`${this.baseUrl}buyer/${username}`, buyer);
-  };
-  
+  }
+
 }

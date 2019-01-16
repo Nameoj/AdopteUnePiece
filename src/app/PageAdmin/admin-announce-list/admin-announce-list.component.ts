1ba079b47f010faac0ec09736ac97a72384652cf
Franck MARTINEZ <pacokris@yahoo.fr>
2019-01-16 14:11:36 +0100

    suppression btn liste annonce

--- src/app/PageAdmin/admin-announce-list/admin-announce-list.component.ts
+++ src/app/PageAdmin/admin-announce-list/admin-announce-list.component.ts
@@ -3,7 +3,7 @@ import { SellerService } from 'src/app/Services/seller.service';
 import { Seller } from 'src/app/models/seller.models';
 import { AnnounceService } from 'src/app/Services/announce.service';
 import { Announce } from 'src/app/models/announce.models';
-import { Router, ActivatedRoute, Params  } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 
 @Component({
@@ -21,18 +21,21 @@ export class AdminAnnounceListComponent implements OnInit {
   sellers;
   seller;
 
-  constructor(private sellerService: SellerService, private announceService: AnnounceService, private route: ActivatedRoute, private router: Router) {}
+  constructor(private sellerService: SellerService,
+    private announceService: AnnounceService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     this.sellers = this.sellerService.sellers;
     console.log(this.sellerEdit);
-    this.route.params.subscribe((params: Params) =>{
+    this.route.params.subscribe((params: Params) => {
       this.username = params['username'];
       this.idx = params['idx'];
       this.seller = this.sellers[this.idx];
-      this.announceService.getSellerAnnounce(this.username).subscribe (
-      response => { this.announces = response; })
-      });
+      this.announceService.getSellerAnnounce(this.username).subscribe(
+        response => { this.announces = response; });
+    });
   }
 
 }
--- src/app/models/announce.models.ts
+++ src/app/models/announce.models.ts
@@ -16,5 +16,5 @@ export class Announce {
         // public endDateModele: string,
         public price: string,
         public charge: number,
-    ) {}
-    }
+    ) { }
+}

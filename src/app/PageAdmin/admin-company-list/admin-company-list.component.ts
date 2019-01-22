b41b9f99d21283cf1ce0fab21c11607693670c2d
Franck MARTINEZ <pacokris@yahoo.fr>
2019-01-22 11:16:00 +0100

    Filtre ca marche presque

--- src/app/PageAdmin/admin-company-list/admin-company-list.component.ts
+++ src/app/PageAdmin/admin-company-list/admin-company-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SellerService } from 'src/app/Services/seller.service';
 import { Seller } from 'src/app/models/seller.models';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-admin-company-list',
@@ -11,9 +12,10 @@ import { Router } from '@angular/router';
 export class AdminCompanyListComponent implements OnInit {
 
   sellers: Seller[];
+  sellersObservable = new BehaviorSubject(this.sellers);
 
   constructor(private sellerService: SellerService, private router: Router) {}
-  
+
   ngOnInit() {
     this.sellerService.getAllSellers().subscribe(
       response => {
@@ -44,5 +46,20 @@ export class AdminCompanyListComponent implements OnInit {
     console.log(idx);
     console.log('See announces of this seller');
     this.router.navigate(['/admin-home/admin-announce-list', username, idx]);
+  }
+
+  indexTrackFn = (index: number) => index;
+  nameTrackFn = (_: number, item: Seller) => item.ville;
+
+  sortBy(prop: 'raisonSociale' | 'ville') {
+    this.sellersObservable.next(this.sellers.map(s => ({ ...s })).sort((a, b) => {
+      const aProp = a[prop], bProp = b[prop];
+      if (aProp < bProp) {
+        return -1;
+      } else if (aProp > bProp) {
+        return 1;
+      }
+      return 0;
+    }));
   }
 }

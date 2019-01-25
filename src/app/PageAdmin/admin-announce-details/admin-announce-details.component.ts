3a594952d7a813b0292971fed9c3428ecabbb41b
AYMERIC <ay.neumann@laposte.net>
2019-01-25 10:15:07 +0100

    admin-announce-details

---
+++ src/app/PageAdmin/admin-announce-details/admin-announce-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-announce-details',
+  templateUrl: './admin-announce-details.component.html',
+  styleUrls: ['./admin-announce-details.component.css']
+})
+export class AdminAnnounceDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
